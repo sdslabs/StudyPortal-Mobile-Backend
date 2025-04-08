5d198c5649345c55202e6bd5b5a97bf39718925a
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2025-04-08 11:25:39 +0530

    Adds env reader
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 
-Dotenv.load()
+Dotenv.load!()
 
 if System.get_env("PHX_SERVER") do
   config :study_portal, StudyPortalWeb.Endpoint, server: true
