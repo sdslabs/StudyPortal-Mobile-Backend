446a5a4bae3e80e5a070af8b331f3b4e75c21971
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2025-08-16 23:33:13 +0530

    small fix in ping controller

--- lib/study_portal_web/controllers/ping_controller.ex
+++ lib/study_portal_web/controllers/ping_controller.ex
@@ -2,8 +2,6 @@ defmodule StudyPortalWeb.PingController do
   use StudyPortalWeb, :controller
 
   def index(conn, _params) do
-    user = conn.assigns.current_user
-    IO.inspect(user, label: "Current User")
     json(conn, %{message: "pong"})
   end
 end
